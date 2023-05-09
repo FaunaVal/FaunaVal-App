81927c367872203493415e99d3a5be2d1a4aa68b
Carolina Torregrosa Nadal <carolatn@gmail.com>
2023-05-09 13:24:08 +0200

    [NEW] image category buttons (and some dependencies)

--- app/src/main/java/com/torregrosa/faunaval/ui/CategoriesActivity.kt
+++ app/src/main/java/com/torregrosa/faunaval/ui/CategoriesActivity.kt
@@ -1,6 +1,8 @@
 package com.torregrosa.faunaval.ui
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -64,6 +67,7 @@ fun Title(modifier: Modifier = Modifier) {
 @Composable
 fun SelectCategoryButton(
     @StringRes labelResourceId: Int,
+    @DrawableRes drawableResId : Int,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -72,13 +76,22 @@ fun SelectCategoryButton(
         modifier = modifier
             .height(140.dp)
             .width(140.dp),
+        contentPadding = PaddingValues(0.dp),
         colors = ButtonDefaults.buttonColors(
             backgroundColor = ButtonColor
         )
     ) {
-        Box(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter){
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+            Image(
+                painter = painterResource(id = drawableResId),
+                contentDescription = null,
+                modifier = Modifier.size(140.dp, 100.dp),
+                alignment = Alignment.TopCenter
+            )
             Text(stringResource(labelResourceId), style = MaterialTheme.typography.h6)
         }
     }
@@ -90,16 +103,16 @@ fun ButtonGrid(){
         horizontalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxWidth()
     ) {
-        SelectCategoryButton(labelResourceId = R.string.mamiferos, onClick = { /*TODO*/ })
-        SelectCategoryButton(labelResourceId = R.string.aves, onClick = { /*TODO*/ })
+        SelectCategoryButton(labelResourceId = R.string.mamiferos, drawableResId = R.drawable.mamifero , onClick = { /*TODO*/ })
+        SelectCategoryButton(labelResourceId = R.string.aves, drawableResId = R.drawable.ave , onClick = { /*TODO*/ })
     }
     Spacer(modifier = Modifier.height(8.dp))
     Row(
         horizontalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier.fillMaxWidth()
     ) {
-        SelectCategoryButton(labelResourceId = R.string.reptiles, onClick = { /*TODO*/ })
-        SelectCategoryButton(labelResourceId = R.string.anfibios, onClick = { /*TODO*/ })
+        SelectCategoryButton(labelResourceId = R.string.reptiles, drawableResId = R.drawable.reptil , onClick = { /*TODO*/ })
+        SelectCategoryButton(labelResourceId = R.string.anfibios, drawableResId = R.drawable.anfibio , onClick = { /*TODO*/ })
     }
 }
 
