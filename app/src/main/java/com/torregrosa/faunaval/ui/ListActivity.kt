3f24fdf03ad816b4c7df63a6e76fd9058ee321f6
Carolina Torregrosa Nadal <carolatn@gmail.com>
2023-06-07 20:14:30 +0200

    [MOD] Loading spinner

--- app/src/main/java/com/torregrosa/faunaval/ui/ListActivity.kt
+++ app/src/main/java/com/torregrosa/faunaval/ui/ListActivity.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,11 +20,11 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.torregrosa.faunaval.AnimalViewModel
+import com.torregrosa.faunaval.R
 import com.torregrosa.faunaval.model.Animal
 import com.torregrosa.faunaval.model.Foto
 import com.torregrosa.faunaval.ui.theme.BackgroundColor
 import com.torregrosa.faunaval.ui.theme.ButtonColor
-import kotlinx.coroutines.runBlocking
 
 
 @Composable
@@ -95,14 +97,40 @@ fun AnimalListScreen(
     filter: String?,
     viewModel: AnimalViewModel = hiltViewModel()
 ) {
-    if (filter != "") {
-        AnimalList(
-            navController,
-            runBlocking { viewModel.getAnimalListFiltered(categoryId, filter) })
-    } else {
-        AnimalList(navController, runBlocking { viewModel.getAnimalList(categoryId) })
+    val animalList by remember {
+        mutableStateOf(mutableListOf<Animal>())
+    }
+    var isLoading by remember {
+        mutableStateOf(true)
     }
 
+    LaunchedEffect(key1 = categoryId, key2 = filter) {
+        if (filter != "") {
+            val filteredList = viewModel.getAnimalListFiltered(categoryId, filter)
+            animalList.clear()
+            animalList.addAll(filteredList)
+        } else {
+            val list = viewModel.getAnimalList(categoryId)
+            animalList.clear()
+            animalList.addAll(list)
+        }
+        isLoading = false
+    }
+
+    Box(Modifier.fillMaxSize()) {
+        if (isLoading) {
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator()
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = stringResource(id = R.string.cargando))
+            }
+        } else {
+            AnimalList(navController, animalList)
+        }
+    }
 }
 
 @Preview(showBackground = true)
